0094142ab90edc5524376d24340def90c750916a
Michal Dziedzic <michal.dziedzic@kuscheholding.com>
2023-12-04 09:11:43 +0100

    refactoring

--- tasks/2023-12-01/index.ts
+++ tasks/2023-12-01/index.ts
@@ -1,22 +1,21 @@
 // Tutaj skopiuj kod zadania
 export class GiftRegistry {
-  gifts: Map<number, string[]>;
+  #gifts: Map<number, string[]>;
 
   constructor() {
-    this.gifts = new Map();
+    this.#gifts = new Map();
   }
 
-  addGift(id_child: number, gift: string) {
-    if (!this.gifts.has(id_child)) {
-      this.gifts.set(id_child, []);
+  addGift(id_child: number, gift: string): void {
+    if (!this.#gifts.has(id_child)) {
+      this.#gifts.set(id_child, []);
     }
 
-    let currentChildList = this.gifts.get(id_child);
-
+    const currentChildList = this.#gifts.get(id_child);
     currentChildList?.push(gift);
   }
-  removeGift(id_child: number, gift: string) {
-    let currentChildList = this.gifts.get(id_child);
+  removeGift(id_child: number, gift: string): void {
+    const currentChildList = this.#gifts.get(id_child);
 
     if (!currentChildList) {
       throw new Error("kid not found");
@@ -29,11 +28,13 @@ export class GiftRegistry {
     }
     currentChildList.splice(indexSearchedGift, 1);
   }
-  getGiftsForChild(id_child: number) {
-    if (!this.gifts.get(id_child)) {
+  getGiftsForChild(id_child: number): Array<string> {
+    const res = this.#gifts.get(id_child);
+
+    if (!res) {
       throw new Error("This list of present for current child doesnt exist");
     }
 
-    return this.gifts.get(id_child);
+    return res;
   }
 }
